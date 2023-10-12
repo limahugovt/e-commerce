33efcbcbdabe9f8b7594df5b776e8b96c04cb604
Victor Hugo <lima.hugo@escolar.ifr.edu.br>
2023-10-12 17:47:27 -0300

    adding components header and footer

---
+++ components/Footer/index.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="w-[1240px] grid gap-[50px] m-auto">
+        <div class="bg-black h-44 rounded-[20px] flex items-center px-16 py-9 justify-between">
+
+            <div class="text-white font-bold text-[40px] max-w-[551px] leading-none	">
+                <h1>STAY UPTO DATE ABOUT OUR LATEST OFFERS</h1>
+            </div>
+
+            <div class="flex flex-col gap-4">
+                <div class="w-[350px] h-12 bg-white rounded-3xl flex py-3 px-4 gap-3">
+                    <img src="@/assets/icons/MailIcon.svg" alt="Mail Icon">
+                    <input type="email" name="email" id="email" placeholder="Enter your email address"
+                        class="outline-none w-full">
+                </div>
+                <div class="w-[350px] h-12 bg-white rounded-3xl flex justify-center items-center py-3 px-4">
+                    <button type="submit">Subscribe to Newsletter</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex justify-between">
+
+            <div class="w-[248px] h-[177px] grid gap-9">
+                <img src="@/assets/icons/Logo.svg" alt="">
+                <p>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+                <img src="@/assets/icons/Social.svg" alt="">
+            </div>
+
+            <div class="grid gap-7">
+                <p class="font-semibold text-base">CONPANY</p>
+                <ul class="grid gap-4">
+                    <li>About</li>
+                    <li>Features</li>
+                    <li>Works</li>
+                    <li>Carrer</li>
+                </ul>
+            </div>
+
+            <div class="grid gap-7">
+                <p class="font-semibold text-base">HELP</p>
+                <ul class="grid gap-4">
+                    <li>Customer Support</li>
+                    <li>Delivery Details</li>
+                    <li>Terms & Conditions</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            
+            <div class="grid gap-7">
+                <p class="font-semibold text-base">FAQ</p>
+                <ul class="grid gap-4">
+                    <li>Acoount</li>
+                    <li>Manege Delivery</li>
+                    <li>Ordes</li>
+                    <li>Payments</li>
+                </ul>
+            </div>
+
+            <div class="grid gap-7">
+                <p class="font-semibold text-base">RESOURCES</p>
+                <ul class="grid gap-4">
+                    <li>Free eBooks</li>
+                    <li>Developmente Tutorialy</li>
+                    <li>How to - Blog</li>
+                    <li>Youtube  Playlist</li>
+                </ul>
+            </div>
+        </div>
+
+        <div>
+            <hr class="border-solid border-b-[1px] border-gray-500">
+            <p class="pt-6">Shop.co © 2000-2023, All Rights Reserved</p>
+        </div>
+    </div>
+</template>
---
+++ components/Header/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="w-screen">
+        <div class="h-9 bg-black text-white flex gap-3 items-center justify-center">
+            <p>Sign up and get 20% off to your first order.</p>
+            <a href="/" class="underline">Sign Up Now</a>
+        </div>
+        <div class="h-24 flex justify-center items-center">
+            <div class="flex gap-10">
+                <img src="@/assets/icons/Logo.svg" alt="Logo">
+
+                <div class="flex gap-6 items-center">
+                    <div class="flex">
+                        <a href="">Shop</a>
+                        <img src="@/assets/icons/ShopArrow.svg" alt="ShopArrow">
+                    </div>
+                    <a href="">On Sale</a>
+                    <a href="">New Arrivals</a>
+                    <a href="">Brands</a>
+                </div>
+
+                <div class="bg-BgSearch h-12 w-[577px] rounded-3xl flex py-3 px-4 gap-3">
+                    <img src="@/assets/icons/SearchIcon.svg" alt="SearchIcon">
+                    <input type="search" name="Search for products..." id="search" placeholder="Search for products..." class="w-full bg-BgSearch outline-0">
+                </div>
+
+                <div class="flex gap-4">
+                    <img src="@/assets/icons/CartIcon.svg" alt="CartIcon">
+                    <img src="@/assets/icons/UserConfig.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
