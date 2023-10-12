e9b5fbeb724d9c9232d61e7a19f3e414702543e7
Victor Hugo <lima.hugo@escolar.ifr.edu.br>
2023-10-12 17:45:40 -0300

    initializate repository

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <Header/>
+    <section class="bg-[url('@/assets/icons/ImageBack.svg')] h-[663px] flex items-center">
+        <div class="ml-[100px]">
+            <div class="max-w-[577px] grid gap-8">
+                <h1 class="font-bold text-[77px] leading-none">FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+                <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+                <div class="bg-black h-[52px] w-[210px] rounded-3xl text-white py-4 px-[54px] flex justify-center items-center">
+                    <button class="">Shop Now</button>
+                </div>
+            </div>
+            <div class="flex gap-8 items-center pt-12">
+                <div class="">
+                    <h2 class="font-bold text-[40px]">200+</h2>
+                    <p>International Brands</p>
+                </div>
+                <div class="h-[74px] bg-black w-[1px]"></div>
+                <div class="">
+                    <h2 class="font-bold text-[40px]">2,000+</h2>
+                    <p>High-Quality Products</p>
+                </div>
+                <div class="h-[74px] bg-black w-[1px]"></div>
+                <div class="">
+                    <h2 class="font-bold text-[40px]">30,000+</h2>
+                    <p>Happy Customers</p>
+                </div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <!-- Imagem -->
+        <div>
+
+        </div>
+    </section>
+    <Footer/>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        BgSearch: '#F0F0F0',
+        BgSection: '#F2F0F1'
+      }
+    },
+    screens: {
+
+    },
+    
+  }
+}
